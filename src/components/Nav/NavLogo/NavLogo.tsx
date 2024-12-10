4b2ce01c769846a40b35aab8f15730f92543f7b1
Chris Ware <c.p.ware@hotmail.com>
2024-12-10 13:11:29 -0700

    Logo update

--- src/components/Nav/NavLogo/NavLogo.tsx
+++ src/components/Nav/NavLogo/NavLogo.tsx
@@ -1,5 +1,6 @@
 import styles from "./NavLogo.module.css";
 import Link from "next/link";
+import Bee from "../../../../public/icons/bee.svg";
 
 interface NavProps {
   color?: string;
@@ -8,7 +9,7 @@ interface NavProps {
 const NavLogo = ({ color = "" }: NavProps) => {
   return (
     <Link href='/' className={`${styles.logo} ${styles[color]}`}>
-      Honey
+      <Bee className={styles.icon} /> Honey
     </Link>
   );
 };
