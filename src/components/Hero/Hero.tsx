b566f27296bc0dc28698fcb96ee014b85def8c1f
Chris Ware <c.p.ware@hotmail.com>
2024-12-11 16:33:31 -0700

    Hero spacing fix

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -3,45 +3,48 @@ import Image from "next/image";
 import HeroImage from "../../../public/images/honey.png";
 import Nav from "../Nav/Nav";
 import Button from "../Button/Button";
+import LayoutWrapper from "../LayoutWrapper";
 
 const Hero = () => {
   return (
-    <div className={styles.parent}>
+    // <div className={styles.parent}>
       <section className={styles.container}>
-        <div className={styles.imgOverlay}></div>
-        <div className={styles.navContainer}>
+        <LayoutWrapper>
+          <div className={styles.imgOverlay}></div>
+          <div className={styles.navContainer}>
             <Nav />
-        </div>
-        <Image
-          src={HeroImage}
-          alt='hero'
-          fill
-          className={styles.img}
-          // priority
-          // sizes='(max-width: 768px) 100vw, (max-width: 1200px) 80vw, 33vw'
-        />
-        <div className={styles.content}>
-          <div className={styles.actualContent}>
-            <h1 className={styles.heading}>
-              Insurance designed for life in motion.
-            </h1>
-            <p className={styles.copy}>
-              Grow your business and enhance your customer experience with
-              embedded insurance solutions made for active and travel markets.
-            </p>
-            <div className={styles.btnContainer}>
-              <Button
-                href='/shop'
-                text='Shop Studio Elite'
-                btnType='yellow'
-                // pulse={true}
-              />
-            </div>
           </div>
-          <div className={styles.right}></div>
-        </div>
+          <Image
+            src={HeroImage}
+            alt='hero'
+            fill
+            className={styles.img}
+            priority
+            sizes='(max-width: 768px) 100vw, (max-width: 1200px) 80vw, 33vw'
+          />
+          <div className={styles.content}>
+            <div className={styles.actualContent}>
+              <h1 className={styles.heading}>
+                Golden drips <br /> of natural delight.{" "}
+              </h1>
+              <p className={styles.copy}>
+                Perfectly unfiltered honey full of rich flavor, it's sweetness
+                you can trust in every drop.
+              </p>
+              <div className={styles.btnContainer}>
+                <Button
+                  href='/shop'
+                  text='Shop Studio Elite'
+                  btnType='yellow'
+                  // pulse={true}
+                />
+              </div>
+            </div>
+            <div className={styles.right}></div>
+          </div>
+        </LayoutWrapper>
       </section>
-    </div>
+    // </div>
   );
 };
 export default Hero;
